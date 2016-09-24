37b4d8c732d7dea6054c51bda3890cf726b8b839
nckng <nng3@stuy.edu>
2016-09-24 16:08:25 -0400

    all done

---
+++ thing.py
@@ -0,0 +1,20 @@
+
+from flask import Flask, render_template
+import occupations
+
+app = Flask(__name__)
+
+def bello():
+    return occupations.OCUPATIONES
+def yello():
+    return occupations.OCUPATIONES.values()
+
+@app.route("/occupations")
+def test_tmplt():
+
+    return render_template( 'model_tmplt.html', foo=occupations.randomizer(),too = bello().items(),they='https://www.wikipedia.org')
+    
+if __name__ == "__main__":
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
